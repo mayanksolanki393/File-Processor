93605d29bdc4dcee4749552d96d5395f4fd63078
unknown <mayankso@mayankso.cybage.com>
2016-06-01 13:26:20 +0530

    third commit

---
+++ test/cyb/jee/excelcmp/dao/impl/test/TestSome.java
@@ -0,0 +1,11 @@
+package cyb.jee.excelcmp.dao.impl.test;
+
+import junit.framework.TestCase;
+
+
+public class TestSome extends TestCase{
+	public void testSome(){
+		System.out.println("Testing some method");
+		assertEquals(1, 1);
+	}
+}
